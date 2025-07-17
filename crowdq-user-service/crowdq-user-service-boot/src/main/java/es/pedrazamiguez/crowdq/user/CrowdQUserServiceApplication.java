10baba9739dd4f8ee0f06199a4b6d5f54664cdc5
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-07-17 18:41:59 +0200

    Configured CrowdQ User Service

---
+++ crowdq-user-service/crowdq-user-service-boot/src/main/java/es/pedrazamiguez/crowdq/user/CrowdQUserServiceApplication.java
@@ -0,0 +1,12 @@
+package es.pedrazamiguez.crowdq.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdQUserServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CrowdQUserServiceApplication.class, args);
+  }
+}
